63ed5434f7d034e69037aa8c7de81d2b12e09060
dm1920 <dm1920@ic.ac.uk>
2022-07-04 13:35:11 +0100

    give a reactive underline to the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="links">
-    <router-link to="/">home</router-link>
-    <router-link to="/about">about</router-link>
-    <router-link to="/project">project</router-link>
-    <router-link to="/cv">cv</router-link>
-    <router-link to="/contact">contact</router-link>
+  <div id="navbar">
+    <div id="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About me</router-link>
+      <router-link to="/project">Projects</router-link>
+      <router-link to="/cv">CV</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <hr id="dividor"/>
   </div>
-  <hr/>
 </template>
 
 <style scoped>
 #links {
-  width: 40rem;
+  width: 50rem;
   height: 3rem;
   margin: auto;
   display: flex;
@@ -20,7 +22,7 @@
 }
 
 a {
-  width: 7rem;
+  width: 9rem;
   height: 2rem;
   color: var(--text-color);
   text-decoration: none;
@@ -29,4 +31,24 @@ a {
   line-height: 2rem;
 }
 
+.router-link-active::after {
+  content: "";
+  display: block;
+  width: calc(9rem - 0.3rem);
+  height: 0;
+  background: var(--primary-color);
+  border: 0.15rem solid var(--primary-color);
+  border-radius: 0.15rem;
+  margin-top: 0.5rem;
+}
+
+#dividor {
+  background: var(--base-color-light);
+  border: 0.15rem solid var(--base-color-light);
+  border-radius: 0.15rem;
+  width: calc(50rem - 0.3rem);
+  height: 0;
+  margin: auto;
+  margin-top: 0.5rem;
+}
 </style>
